5f8c793591bd38aa0e4ed656f9d1ee252e40c880
Luigi <luigismilzo@live.com>
2020-03-26 16:10:55 +0100

    Script + HTML

---
+++ javascript/main.js
@@ -0,0 +1,23 @@
+// Variables listing
+var pricePerKm = 0.21;
+var distance = parseInt(prompt('Quanti chilometri vuoi percorrere?'));
+var age = parseInt(prompt('Quanti anni ha il passeggero?'));
+var basePrice = distance * pricePerKm;
+var discountYoung = basePrice * 0.8;
+var discountSenior = basePrice * 0.6;
+
+// Conditional statements
+if (age >= 65) {
+  document.getElementById('discount').innerHTML = 'Prezzo scontato per over 65: <br>' + discountSenior + ' €';
+}
+else if (age < 18) {
+  document.getElementById('discount').innerHTML = 'Prezzo scontato per under 18: <br>' + discountYoung + ' €';
+}
+else {
+  document.getElementById('no-discount').innerHTML = 'Prezzo del biglietto: <br>' + basePrice + ' €';
+}
+
+// HTML inclusions 
+document.getElementById('distance').innerHTML = distance + ' Km';
+document.getElementById('age').innerHTML = age;
+
